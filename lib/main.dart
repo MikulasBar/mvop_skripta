14ec2f52c7f498e925fe7e83390ceb5545242922
Mikulas Bartacek <mikulasbartac@gmail.com>
2026-05-21 12:11:28 +0200

    comment

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  // comment
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
